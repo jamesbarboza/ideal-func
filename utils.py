e41ad9183cff5b3e2f5424688eafa6a10607bbc2
James Barboza <jamesbarboza720@gmail.com>
2022-03-19 18:48:32 +0530

    utils.py added

---
+++ utils.py
@@ -0,0 +1,17 @@
+from bokeh.plotting import output_file, figure, show
+from bokeh.layouts import row
+
+def get_a_plotted_line(x = [], y = [], plot_name = ""):
+  output_file("html/" + plot_name + ".html")
+  fig = figure(title = plot_name)
+  fig.line(x, y)
+  return fig
+
+def get_lines_to_plot(table_data_object):
+  training_lines = table_data_object.get_coordinates()
+  plotted_lines = []
+  for i in range(len(training_lines)):
+    x = training_lines[i][0]
+    y = training_lines[i][1]
+    plotted_lines.append(get_a_plotted_line(x, y, "training-" + str(i)))
+  return plotted_lines
